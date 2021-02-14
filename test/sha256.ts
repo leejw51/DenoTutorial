6e047e49bf980acdbd93295aaf8d46a93f2c9fc7
jonghwan <jonghwan@crypto.com>
2021-02-14 20:57:15 +0900

    sha256

--- test/sha256.ts
+++ test/sha256.ts
@@ -1,3 +1,5 @@
 import { sha256 } from "https://denopkg.com/chiefbiiko/sha256@v1.0.2/mod.ts";
+import { Buffer } from "https://deno.land/std/node/buffer.ts";
 
-console.log('SHA2-256 of ""', sha256("", "utf8", "hex"));
+const a = Buffer.from([1, 2, 3]);
+console.log(`SHA2-256 of ${JSON.stringify(a)}`, sha256(a, "utf8", "hex"));
